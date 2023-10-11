918ab5a229ea949c6ab6d828c413433dea6659ed
Rahul Dey <rahuldey832000@gmail.com>
2023-10-11 09:53:23 +0530

    Add Backend to Full Stack Deployment

---
+++ FullStackDeployment/backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+import { jokes } from "./jokes.js";
+
+// create a new instance of express
+const app = express();
+
+// add middleware for log all request data to console
+app.use((req, res, next) => {
+  console.log(
+    `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} ${
+      req.method
+    } ${req.url}`
+  );
+  next();
+});
+
+// defult get routes
+app.get("/", (req, res) => {
+  res.send("Server is ready to serve!");
+});
+
+// add get jokes routes
+app.get("/jokes", (req, res) => {
+  res.send(jokes);
+});
+
+// define server port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
