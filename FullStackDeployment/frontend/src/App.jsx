5913de8bdb0313e9d06ac848b80fca75993823b3
Rahul Dey <rahuldey832000@gmail.com>
2023-10-11 09:53:46 +0530

    Add Frontend to Full stack Deployment

---
+++ FullStackDeployment/frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  return (
+    <>
+      <h1>Chai our Full stack</h1>
+      <p>JOKES : {jokes.length}</p>
+
+      {jokes.map((joke) => {
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>;
+      })}
+    </>
+  );
+}
+
+export default App;
