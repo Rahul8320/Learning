ebc206a1ea6dc5e0e6c16d89cc29d53514fa8a7f
Rahul Dey <rahuldey832000@gmail.com>
2023-10-14 09:23:55 +0530

    Creating a simple node app in docker

---
+++ LearningDocker/SimpleNodeApp/jokes.js
@@ -0,0 +1,29 @@
+// give me 5 joke of object array with content
+
+export const jokes = [
+  {
+    id: 1,
+    title: "A first joke",
+    content: "This is first joke content",
+  },
+  {
+    id: 2,
+    title: "A second joke",
+    content: "This is second joke content",
+  },
+  {
+    id: 3,
+    title: "A third joke",
+    content: "This is third joke content",
+  },
+  {
+    id: 4,
+    title: "A fourth joke",
+    content: "This is fourth joke content",
+  },
+  {
+    id: 5,
+    title: "A fifth joke",
+    content: "This is fifth joke content",
+  },
+];
