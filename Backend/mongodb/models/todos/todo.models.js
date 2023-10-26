0185e062f3ee300532e0c15e00d6f1ef07f809e8
RAHUL DEY <rahuldey832000@gmail.com>
2023-10-26 17:08:19 +0000

    modified todo model

--- Backend/mongodb/models/todos/todo.models.js
+++ Backend/mongodb/models/todos/todo.models.js
@@ -2,7 +2,19 @@
 import mongoose from "mongoose";
 
 // create the schema
-const todoSchema = new mongoose.Schema({});
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 // export the model
 export const Todo = mongoose.model("Todos", todoSchema);
